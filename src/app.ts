24d9b107612b0d823e70f7727775764efb185144
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-23 16:38:43 -0300

    feat: router /add-match

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from "express"
 import { guessesRouter } from "./routes/guesses.routes.js";
+import { matchesRouter } from "./routes/matches.routes.js";
 import { usesRouter } from "./routes/user.routes.js";
 
 const server = express();
 
 server
     .use(express.json())
+    .get("/health", (req, res) => res.send("OK!"))
     .use(usesRouter)
     .use(guessesRouter)
+    .use(matchesRouter)
 
-server.get("/health", (req, res) => res.send("OK!"));
 const port = 4000
 server.listen(port, () => console.log(`Server is running in ${port}`))
---
+++ src/routes/matches.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const matchesRouter = Router();
+matchesRouter.post("/add-mathc")
+export{matchesRouter};
